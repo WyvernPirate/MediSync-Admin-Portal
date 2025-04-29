e67f868d0895120f854a27b4264de5bc17c1b93d
WyvernPirate <moloieric@gmail.com>
2025-04-29 16:31:23 +0200

    put in security measures

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRFSrbAFOQSDAqaY8kRK2JO8urT24eVHk",
-  authDomain: "doctorappointmentapp-9f11a.firebaseapp.com",
-  projectId: "doctorappointmentapp-9f11a",
-  storageBucket: "doctorappointmentapp-9f11a.firebasestorage.app",
-  messagingSenderId: "4258195571",
-  appId: "1:4258195571:web:9f95e3008d6fa4b0ece206",
-  measurementId: "G-TNRHQER43F"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
